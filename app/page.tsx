224d55664c3864290e48fe8273c51f04d1246cff
dieguscl <diegocastrolaya@gmail.com>
2025-03-06 17:24:54 -0500

    refactor: change title

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { WeightCalculatorForm } from "./components/weight-calculator-form"
 export default function Home() {
   return (
     <main className="pt-14 container mx-auto p-4 flex flex-col items-center gap-12">
-      <TypographyH1>Calculadora de pesos</TypographyH1>
+      <TypographyH1>Barbell Calculator</TypographyH1>
       <WeightCalculatorForm />
     </main>
   )
